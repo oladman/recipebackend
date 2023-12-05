d8a3ab42ea9cf695d6ffff636d64520c00539f72
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2023-12-05 14:30:59 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConnect = require("./mongodb");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const Recipe = require("./models/model");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+mongoose
+  .connect(
+    "mongodb+srv://oladman:vIRJDYpFPoadswd8@cluster0.vebwjm0.mongodb.net/recipe"
+  )
+  .then(() => {
+    app.listen(8081, () => {
+      console.log("connected to MongoDb");
+      console.log("Server Running on port 8081");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.post("/create", async (req, res) => {
+  console.log(req.body);
+  try {
+    const recipe = await Recipe.create(req.body);
+    res.status(200).json(Recipe);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/getRecipe", (req, res) => {
+  Recipe.find({})
+    .then(function (recipe) {
+      res.json(recipe);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+
+
+
+app.get("/gethotone", (req, res) => {
+  Recipe.find({}).limit(1)
+    .then(function (recipe) {
+      res.json(recipe);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.get("/gethotseven", (req, res) => {
+  Recipe.find({}).limit(7)
+    .then(function (recipe) {
+      res.json(recipe);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.get("/getRecipe/:id", (req, res) => {
+  const { id } = req.params;
+  Recipe.findById(id)
+    .then(function (recipe) {
+      res.json(recipe);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const recipe = await Recipe.findByIdAndUpdate(id, req.body);
+    if (!recipe) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any recipe with ID ${id}` });
+    }
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const recipe = await Recipe.findByIdAndDelete(id);
+    if (!recipe) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any recipe with ID ${id}` });
+    }
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
---
+++ models/model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema(
+  {
+    Title: {
+      type: String,
+      required: [true, "Please enter a product name"],
+    },
+
+    Description: {
+      type: String,
+      required: false
+    },
+    
+    
+    ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+
+    Body: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Recipe = mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema);
+
+module.exports = mongoose.model("Recipe", recipeSchema);
